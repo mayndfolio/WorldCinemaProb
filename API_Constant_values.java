481fdea4553bc2275a246dafee9c764f2d445d8a
mayndfolio <58941465+mayndfolio@users.noreply.github.com>
2021-12-14 06:49:13 +0300

    Create API_Constant_values.java

---
+++ API_Constant_values.java
@@ -0,0 +1,6 @@
+package com.example.worldcinemaprob;
+
+public class API_Constant_values {
+    protected final String API_URL_BASE = "http://cinema.areas.su/";
+    protected final String API_AUTH_REGISTER = "auth/register";
+}
